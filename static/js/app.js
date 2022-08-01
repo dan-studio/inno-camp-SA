33d687e3aa4cc45b3a402d56e1a3986fb3408fd9
dan-studio <danstudiocode@gmail.com>
2022-08-01 16:37:05 +0900

    navbar complete

--- static/js/app.js
+++ static/js/app.js
@@ -2,15 +2,29 @@ function signup() {
   let id = $('#id').val()
   let pw = $('#pw').val()
   $.ajax({
-      type: "POST",
-      url: "/signup",
-      data: {
-        id_give: id, 
-        pw_give: pw
-      },
-      success: function (response) {
-          alert(response["msg"])
-          window.location.reload()
-      }
+    type: "POST",
+    url: "/signup",
+    data: {
+      id_give: id,
+      pw_give: pw
+    },
+    success: function (response) {
+      alert(response["msg"])
+      window.location.reload()
+    }
   });
+}
+
+const toggle = document.getElementById('toggle');
+const sidebar = document.getElementById('sidebar');
+
+toggle.onclick = function () {
+  toggle.classList.toggle('active');
+  sidebar.classList.toggle('active');
+}
+document.onclick = function (e) {
+  if (e.target.id !== 'sidebar' && e.target.id !== 'toggle') {
+    toggle.classList.remove('active')
+    sidebar.classList.remove('active')
+  }
 }
